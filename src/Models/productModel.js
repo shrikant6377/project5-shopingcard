93081571a4d1d4ca3641f4e2936be919caeae8dd
shrikant <kantshri561@gmail.com>
2022-06-14 07:46:00 +0530

    final code

--- src/Models/productModel.js
+++ src/Models/productModel.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    title: {
-        type: String, 
-        required:true,  
-        unique: true,
-        uppercase:true
-    },
+    title: {type: String,required:true, unique: true,uppercase:true},
   description: {
       type: String, 
       required:true, 
