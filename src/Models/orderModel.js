2422e4db7fbe31a127e41369f82c5741663e2e1d
shrikant <kantshri561@gmail.com>
2022-06-14 07:45:01 +0530

    final code

--- src/Models/orderModel.js
+++ src/Models/orderModel.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 const ObjectId =  mongoose.Schema.Types.ObjectId
 
-
 const orderSchema = new mongoose.Schema(
     {
-    userId: {type:ObjectId,ref : 'User',required: true
-    },
+    userId: {type:ObjectId,ref : 'User',required: true},
     items: [{
-      productId: { type:ObjectId,ref :'Product',required:true},
+productId: { type:ObjectId,ref :'Product',required:true},
       quantity: { type:Number, required: true, min: 1},
         _id:false
     }],
